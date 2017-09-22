e4892b6bb09946aa302c1bbb0b5c6c3fa32070bb
Sam Martin <smartin@sdl.com>
2017-09-22 13:45:05 +0100

    Initial commit

---
+++ ChefHelpers/Public/New-CHKnifeFile.ps1
@@ -0,0 +1,29 @@
+﻿Function Create-KnifeFile{
+    param(
+        [Parameter(Mandatory)]
+        $OrganisationName,
+        [Parameter(Mandatory)]
+        $Username,
+        [Parameter(Mandatory)]
+        $Cookbookpath = "#{current_dir}/cookbooks",
+        [Parameter(Mandatory)]
+        $ChefServerUri = "https://api.chef.io",
+        [Parameter(Mandatory)]
+        $KnifeFileName = 'knife.rb'
+    )
+
+
+    Write-Verbose "Writing knife file to $KnifeFileName"
+    @"
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "$Username"
+client_key               "#{current_dir}/$ClientKey"
+chef_server_url          "$ChefServerUri/organizations/$OrganisationName"
+cookbook_path            ["$($Cookbookpath -join '","')"]
+"@ | Set-Content $KnifeFileName
+
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Chef Helpers
+This module contains PowerShell cmdlets to automate common tasks when working with Chef in PowerShell.
+
+If you're interested in this module you should make sure you're using the Chef PowerShell wrappers which make calling knife, chef, etc. in PowerShell much easier. This module is installed in `C:\opscode\chefdk\modules\chef\` when you install ChefDK.
+
+- Sam Martin (samjackmartin@gmail.com)
