826f959a2a7422d79f475bb51b56f6c57f092d91
Sam Martin <smartin@sdl.com>
2017-09-26 15:47:28 +0100

    Added Get-CHEncryptedDatabag

---
+++ ChefHelpers/Public/Get-CHEncryptedDatabag.ps1
@@ -0,0 +1,14 @@
+﻿Function Get-CHEncryptedDatabag{
+    param(
+        [parameter(Mandatory)]
+        $knifeRb = ".\knife.rb",
+        [parameter(Mandatory)]
+        $SecretFile, 
+        [parameter(Mandatory)]
+        $DatabagName, 
+        [parameter(Mandatory)]
+        $DatabagItemName
+    )
+    knife data bag show "$DatabagName" "$DatabagItemName" -c $knifeRB --secret-file $SecretFile -F json
+    $DatabagItem  -replace '^\w*[^"{}\[\]]+' | Convertfrom-Json
+}
